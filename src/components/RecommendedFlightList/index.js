8eadd3087aba6a219a528284c06fd97b82e98541
Paulo Vaz <paulopontovaz@gmail.com>
2021-08-16 18:53:27 +0200

    Mover os <tr>s para seu próprio componente

--- src/components/RecommendedFlightList/index.js
+++ src/components/RecommendedFlightList/index.js
@@ -1,88 +1,39 @@
+import RecommendedFlightListItem from "../RecommendedFlightListItem";
 import "./styles.css";
 
-const RecommendedFlights = ({ destinationsArray }) => {
-  return (
-    <div className="recommended-flight-list">
-      <div className="recommended-flight-list-table-container">
-        <table className="recommended-flight-list-table">
-          <thead>
-            <tr>
-              <th>País</th>
-              <th>
-                Número de Casos
-                <br />
-                (de COVID-19)
-              </th>
-              <th>
-                Taxa de Crescimento
-                <br />
-                (de COVID-19)
-              </th>
-              <th>Preço</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* {
-						"destinationIata":"MIA",
-						"price":"1931.86",
-						"currency":"BRL",
-						"countryCode":"US",
-						"cases":33625419,
-						"contamination_rate":0.0001,
-						"country_name":"USA",
-						"flag_link":"https://www.countryflags.io/US/flat/64.png"} 
-						*/}
-            {destinationsArray &&
-              destinationsArray.map(
-                ({
-                  countryCode,
-                  country_name,
-                  cases,
-                  contamination_rate,
-                  price,
-                  currency,
-                  flag_link,
-                }) => (
-                  <tr key={countryCode}>
-                    <td
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "10px",
-                      }}
-                    >
-                      <img
-                        src={flag_link}
-                        alt={`${countryCode} flag`}
-                        width={45}
-                        height={45}
-                      />
-                      <p>{country_name}</p>
-                    </td>
-                    <td>{cases}</td>
-                    <td
-                      className={
-                        contamination_rate > 0
-                          ? "green"
-                          : contamination_rate < 0
-                          ? "red"
-                          : ""
-                      }
-                    >
-                      {contamination_rate > 0
-                        ? `+${contamination_rate}`
-                        : contamination_rate}
-                    </td>
-                    <td>{`${price} ${currency.toUpperCase()}`}</td>
-                  </tr>
-                )
-              )}
-          </tbody>
-        </table>
-      </div>
-      <div className="recommended-flight-list-footer"></div>
-    </div>
-  );
+const RecommendedFlights = ({ destinations = [] }) => {
+	return (
+		<div className="recommended-flight-list">
+			<div className="recommended-flight-list-table-container">
+				<table className="recommended-flight-list-table">
+					<thead>
+						<tr>
+							<th>País</th>
+							<th>
+								Número de Casos
+								<br />
+								(de COVID-19)
+							</th>
+							<th>
+								Taxa de Crescimento
+								<br />
+								(de COVID-19)
+							</th>
+							<th>Preço</th>
+						</tr>
+					</thead>
+					<tbody>
+						{destinations.map((destination) => (
+							<RecommendedFlightListItem
+								destination={destination}
+							/>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<div className="recommended-flight-list-footer" />
+		</div>
+	);
 };
 
 export default RecommendedFlights;
--- src/components/RecommendedFlightListItem/index.js
+++ src/components/RecommendedFlightListItem/index.js
@@ -1,7 +1,37 @@
 import "./styles.css";
 
-const RecommendedFlightListItem = () => {
-	return <div className=""></div>;
-};
+const RecommendedFlightListItem = ({
+	destination: {
+		countryCode,
+		country_name,
+		cases,
+		contamination_rate,
+		price,
+		currency,
+		flag_link,
+	},
+}) => (
+	<tr key={countryCode} className="recommended-flight-list-item">
+		<td>
+			<img src={flag_link} alt={`${countryCode} flag`} />
+			<p>{country_name}</p>
+		</td>
+		<td>{cases}</td>
+		<td
+			className={
+				contamination_rate > 0
+					? "green"
+					: contamination_rate < 0
+					? "red"
+					: ""
+			}
+		>
+			{contamination_rate > 0
+				? `+${contamination_rate}`
+				: contamination_rate}
+		</td>
+		<td>{`${price} ${currency.toUpperCase()}`}</td>
+	</tr>
+);
 
 export default RecommendedFlightListItem;
