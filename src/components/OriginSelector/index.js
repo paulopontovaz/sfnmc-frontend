3d5cf379fcb89c44444b4f5936a1f4604c6dfa5f
gstadtler <ghstadtler@gmail.com>
2021-08-15 22:56:23 -0300

    POST request no server para enviar origem do user

--- src/components/OriginSelector/index.js
+++ src/components/OriginSelector/index.js
@@ -1,63 +1,55 @@
 import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import "./styles.css";
 
 const OriginSelector = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-	const onSubmit = (data) => {
-		console.log(data);
-	};
+  const onSubmit = async (data) => {
+    const response = await axios.post("http://localhost:3333/", {
+      iata_from: data.origin_name,
+    });
+    console.log(response);
+  };
 
-	return (
-		<div className="origin-selector">
-			<form
-				onSubmit={handleSubmit(onSubmit)}
-				className="origin-selector-form"
-			>
-				<div className="origin-selector-input-container">
-					<label
-						className="origin-selector-label"
-						htmlFor="origin-name"
-					>
-						Saindo de
-					</label>
-					<input
-						className="origin-selector-input"
-						placeholder="Nome da sua cidade"
-						list="origin-list"
-						{...register("origin-name", { required: true })}
-					/>
-					<datalist id="origin-list">
-						{[
-							"Chocolate",
-							"Coconut",
-							"Mint",
-							"Strawberry",
-							"Vanilla",
-						].map((item) => (
-							<option key={item}>{item}</option>
-						))}
-					</datalist>
-					{errors.exampleRequired && (
-						<span className="origin-selector-error">
-							Campo obrigatório
-						</span>
-					)}
-				</div>
-				<div className="origin-selector-button-container">
-					<button type="submit" className="origin-selector-button">
-						<FontAwesomeIcon icon={faSearchLocation} size="2x" />
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className="origin-selector">
+      <form onSubmit={handleSubmit(onSubmit)} className="origin-selector-form">
+        <div className="origin-selector-input-container">
+          <label className="origin-selector-label" htmlFor="origin_name">
+            Saindo de
+          </label>
+          <input
+            className="origin-selector-input"
+            placeholder="Nome da sua cidade"
+            list="origin-list"
+            {...register("origin_name", { required: true })}
+          />
+          <datalist id="origin-list">
+            {["Chocolate", "Coconut", "MUC", "Strawberry", "Vanilla"].map(
+              (item) => (
+                <option key={item}>{item}</option>
+              )
+            )}
+          </datalist>
+          {errors.exampleRequired && (
+            <span className="origin-selector-error">Campo obrigatório</span>
+          )}
+        </div>
+        <div className="origin-selector-button-container">
+          <button type="submit" className="origin-selector-button">
+            <FontAwesomeIcon icon={faSearchLocation} size="2x" />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default OriginSelector;
