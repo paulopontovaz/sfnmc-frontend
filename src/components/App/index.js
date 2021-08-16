bb49a8cc510d84ea74d9d27ddd10bd537dcbdd36
gstadtler <ghstadtler@gmail.com>
2021-08-16 00:59:18 -0300

    Conecta com WS server e streama os dados

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,63 @@
+import { useEffect, useState } from "react";
 import AppHeader from "../AppHeader";
 import OriginSelector from "../OriginSelector";
 import RecommendedFlightList from "../RecommendedFlightList";
 import "./styles.css";
 
+const connection = new WebSocket("ws://localhost:5000");
+connection.binaryType = "arraybuffer";
+
 const App = () => {
-	return (
-		<div className="app">
-			<AppHeader />
-			<OriginSelector />
-			<RecommendedFlightList />
-		</div>
-	);
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    connection.onopen = () => {
+      console.log("Connected with server");
+      connection.send("Hi, I am connected to you now hihi");
+    };
+
+    connection.onclose = () => {
+      console.log("Connection ended.");
+    };
+
+    connection.onmessage = ({ data }) => {
+      let tempDestinations = [];
+
+      // converte o buffer que vem da fila em uma string
+      const converter = new TextDecoder("utf-8");
+      const convertedData = converter.decode(data);
+
+      // converte a string em um objeto
+      const data2obj = JSON.parse(convertedData);
+
+      destinations.forEach((destination) => {
+        tempDestinations.push(destination);
+      });
+
+      const index = tempDestinations.findIndex((element) => {
+        return element.countryCode === data2obj.countryCode;
+      });
+
+      console.log("index", index);
+
+      if (index === -1) {
+        tempDestinations.push(data2obj);
+      } else {
+        tempDestinations[index] = data2obj;
+      }
+
+      setDestinations(tempDestinations);
+      console.log("at finish", destinations);
+    };
+  }, [destinations]);
+
+  return (
+    <div className="app">
+      <AppHeader />
+      <OriginSelector />
+      <RecommendedFlightList destinationsArray={destinations} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/RecommendedFlightList/index.js
+++ src/components/RecommendedFlightList/index.js
@@ -1,111 +1,88 @@
 import "./styles.css";
 
-const mockData = [
-	{
-		id: 1,
-		country: "Germany",
-		cases: 500000,
-		growth: -3.5,
-		price: 3000,
-		currency: "USD",
-	},
-	{
-		id: 2,
-		country: "Brazil",
-		cases: 500000,
-		growth: 3.5,
-		price: 2000,
-		currency: "USD",
-	},
-	{
-		id: 3,
-		country: "United States",
-		cases: 1000000,
-		growth: -3.5,
-		price: 3000,
-		currency: "USD",
-	},
-	{
-		id: 4,
-		country: "Germany",
-		cases: 500000,
-		growth: -3.5,
-		price: 3000,
-		currency: "USD",
-	},
-	{
-		id: 5,
-		country: "Brazil",
-		cases: 500000,
-		growth: 3.5,
-		price: 2000,
-		currency: "USD",
-	},
-	{
-		id: 6,
-		country: "United States",
-		cases: 1000000,
-		growth: -3.5,
-		price: 3000,
-		currency: "USD",
-	},
-];
-
-const RecommendedFlights = () => {
-	return (
-		<div className="recommended-flight-list">
-			<div className="recommended-flight-list-table-container">
-				<table className="recommended-flight-list-table">
-					<thead>
-						<tr>
-							<th>País</th>
-							<th>
-								Número de Casos
-								<br />
-								(de COVID-19)
-							</th>
-							<th>
-								Taxa de Crescimento
-								<br />
-								(de COVID-19)
-							</th>
-							<th>Preço</th>
-						</tr>
-					</thead>
-					<tbody>
-						{mockData.map(
-							({
-								id,
-								country,
-								cases,
-								growth,
-								price,
-								currency,
-							}) => (
-								<tr key={id}>
-									<td>{country}</td>
-									<td>{cases}</td>
-									<td
-										className={
-											growth > 0
-												? "green"
-												: growth < 0
-												? "red"
-												: ""
-										}
-									>
-										{growth > 0 ? `+${growth}` : growth}
-									</td>
-									<td>{`${price} ${currency.toUpperCase()}`}</td>
-								</tr>
-							)
-						)}
-					</tbody>
-				</table>
-			</div>
-			<div className="recommended-flight-list-footer"></div>
-		</div>
-	);
+const RecommendedFlights = ({ destinationsArray }) => {
+  return (
+    <div className="recommended-flight-list">
+      <div className="recommended-flight-list-table-container">
+        <table className="recommended-flight-list-table">
+          <thead>
+            <tr>
+              <th>País</th>
+              <th>
+                Número de Casos
+                <br />
+                (de COVID-19)
+              </th>
+              <th>
+                Taxa de Crescimento
+                <br />
+                (de COVID-19)
+              </th>
+              <th>Preço</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {
+						"destinationIata":"MIA",
+						"price":"1931.86",
+						"currency":"BRL",
+						"countryCode":"US",
+						"cases":33625419,
+						"contamination_rate":0.0001,
+						"country_name":"USA",
+						"flag_link":"https://www.countryflags.io/US/flat/64.png"} 
+						*/}
+            {destinationsArray &&
+              destinationsArray.map(
+                ({
+                  countryCode,
+                  country_name,
+                  cases,
+                  contamination_rate,
+                  price,
+                  currency,
+                  flag_link,
+                }) => (
+                  <tr key={countryCode}>
+                    <td
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "10px",
+                      }}
+                    >
+                      <img
+                        src={flag_link}
+                        alt={`${countryCode} flag`}
+                        width={45}
+                        height={45}
+                      />
+                      <p>{country_name}</p>
+                    </td>
+                    <td>{cases}</td>
+                    <td
+                      className={
+                        contamination_rate > 0
+                          ? "green"
+                          : contamination_rate < 0
+                          ? "red"
+                          : ""
+                      }
+                    >
+                      {contamination_rate > 0
+                        ? `+${contamination_rate}`
+                        : contamination_rate}
+                    </td>
+                    <td>{`${price} ${currency.toUpperCase()}`}</td>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+      </div>
+      <div className="recommended-flight-list-footer"></div>
+    </div>
+  );
 };
 
 export default RecommendedFlights;
