7e42c77fbd8abcf6487477ff23ac7caec1538ea3
Paulo Vaz <paulopontovaz@gmail.com>
2021-08-17 23:09:57 +0200

    Remover debugger

--- src/store/flightRecommendationSlice.js
+++ src/store/flightRecommendationSlice.js
@@ -57,8 +57,6 @@ export const submitSearch =
 					dispatch(setRecommendedFlights(data2obj));
 					dispatch(setIsLoading(false));
 
-					debugger;
-
 					if (!data2obj?.length) {
 						dispatch(
 							setMessage({
