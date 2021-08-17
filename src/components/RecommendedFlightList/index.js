975a5db6012e1680818621514d0d65339f0b29f9
Paulo Vaz <paulopontovaz@gmail.com>
2021-08-17 17:56:11 +0200

    Adicionar componente para mostrar que a página está carregando

--- src/components/RecommendedFlightList/index.js
+++ src/components/RecommendedFlightList/index.js
@@ -1,35 +1,58 @@
+import { useSelector } from "react-redux";
+import { css } from "@emotion/react";
+import BarLoader from "react-spinners/BarLoader";
 import RecommendedFlightListItem from "../RecommendedFlightListItem";
 import "./styles.css";
 
-const RecommendedFlights = ({ destinations = [] }) => (
-	<div className="recommended-flight-list">
-		<div className="recommended-flight-list-table-container">
-			<table className="recommended-flight-list-table">
-				<thead>
-					<tr>
-						<th>País</th>
-						<th>
-							Número de Casos
-							<br />
-							(de COVID-19)
-						</th>
-						<th>
-							Taxa de Crescimento
-							<br />
-							(de COVID-19)
-						</th>
-						<th>Preço</th>
-					</tr>
-				</thead>
-				<tbody>
-					{destinations.map((destination) => (
-						<RecommendedFlightListItem destination={destination} />
-					))}
-				</tbody>
-			</table>
+const loaderCss = css`
+	width: 100%;
+	align-self: center;
+`;
+
+const RecommendedFlights = ({ destinations = [] }) => {
+	const isLoading = useSelector(
+		(state) => state.flightRecommendation.isLoading
+	);
+	return (
+		<div className="recommended-flight-list">
+			<div className="recommended-flight-list-table-container">
+				<table className="recommended-flight-list-table">
+					<thead>
+						<tr>
+							<th>País</th>
+							<th>
+								Número de Casos
+								<br />
+								(de COVID-19)
+							</th>
+							<th>
+								Taxa de Crescimento
+								<br />
+								(de COVID-19)
+							</th>
+							<th>Preço</th>
+						</tr>
+					</thead>
+					<BarLoader
+						loading={isLoading}
+						color="#d66853"
+						size={250}
+						css={loaderCss}
+					/>
+					{destinations.length > 0 && (
+						<tbody>
+							{destinations.map((destination) => (
+								<RecommendedFlightListItem
+									destination={destination}
+								/>
+							))}
+						</tbody>
+					)}
+				</table>
+			</div>
+			<div className="recommended-flight-list-footer" />
 		</div>
-		<div className="recommended-flight-list-footer" />
-	</div>
-);
+	);
+};
 
 export default RecommendedFlights;
